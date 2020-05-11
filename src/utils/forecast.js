b250b1e0ac1a5f653a048c142ae538d3d9f86074
Russell Gregory <russellgregory0@gmail.com>
2020-05-11 13:00:45 +1000

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=56bf8c7ebd51c809e2c3c006c1a33a18&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=m'
+
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
+            callback('Could not access Forecast Service', undefined)
+        } else if (body.error) {
+            callback('Invalid Coordinates', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,27 @@
+const request = require('postman-request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidmFuaWxsYXNsaWNlIiwiYSI6ImNrOWRvcnN3cTA1eHczbXA2aXA3ejJ6cTcifQ.9s43Zl_9Ha5XVDQFH1oqTg&limit=1'
+
+    request({ url, json: true }, (error, { body } = {}) => {
+
+        if (error) {
+            callback('Unable to connect to Geocoding Service', undefined)
+        } else if (!body.features.length > 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+// geocode('Melbourne', (error, data) => {
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
+
+module.exports = geocode
