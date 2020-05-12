56617ae89818c131af26db5aff7860c107f22d6b
Russell Gregory <russellgregory0@gmail.com>
2020-05-12 13:07:16 +1000

    Adding Feels Like to Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Invalid Coordinates', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain. It feels like ' + body.current.feelslike + ' degrees.')
+            console.log(body)
         }
     })
 }
